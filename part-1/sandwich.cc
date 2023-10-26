610771c8637313e921bae0d311b9a56e2b95d1f2
YeonJungHoon <99351926+YeonJungHoon@users.noreply.github.com>
2023-10-26 03:14:58 +0000

    first commit

--- part-1/sandwich.cc
+++ part-1/sandwich.cc
@@ -1,25 +1,34 @@
 // Nathan Wong
 // potatotatertoot@csu.fullerton.edu
 // @potatotatertoot
-// Partners: @KevinP05, @YeonJungHoon
+// Partners: @YeonJungHoon, @KevinP05
 
 #include <iostream>
 #include <string>
 #include <vector>
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> arguments{argv+1, argv + argc};
+  std::vector<std::string> arguments{argv, argv + argc};
 
-  if (arguments.size() != 3) {
-    std::cout << "error: you must supply three arguments";
-    return -1;
+  // TODO: Validate that the number of arguments is correct.
+  // If not, print an error message and return a non-zero value.
+  if (arguments.size() != 4) {
+    std::cout << " error: you must supply three arguments ";
+    return 1;
   }
-
-  std::string protein{arguments[1]};
-  std::string bread{arguments[2]};
-  std::string condiment{arguments[3]};
-
-  std::cout << "Your order: A" << protein << "sandwich on" << bread << "with" << condiment;
-
+  // TODO: Declare three std::string variables to hold the
+  // protein, bread, and condiment input.
+  // Initialize each variable with an element of the arguments vector
+  // declared above.
+  std::string command{arguments.at(0)};
+  std::string protein{arguments.at(1)};
+  std::string bread{arguments.at(2)};
+  std::string condiment{arguments.at(3)};
+  // TODO: Use cout to print output following the pattern
+  // Your order: A *PROTEIN* sandwich on *BREAD* with *CONDIMENT*.
+  // on its own line.
+  std::cout << "Your order: "
+            << " A " << protein << " sandwich on " << bread << " with "
+            << condiment << "\n";
   return 0;
 }
